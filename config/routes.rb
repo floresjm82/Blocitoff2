c653d35dd0e43a41e70c2b01a40ea0f00f8c2848
Jorge <floresjmjr@gmail.com>
2015-12-11 17:49:41 -0500

    generated items controller, added confirmable and lockout

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :items, only: [:create]
+  end
 
   get 'structure/main_page'
 
