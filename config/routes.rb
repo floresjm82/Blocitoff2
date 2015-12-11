bc20690c202696bd0527e5f19234d9a2dcdf3e5d
Jorge <floresjmjr@gmail.com>
2015-12-10 20:32:12 -0500

    made the user show the root

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show]
 
-  root 'structure#main_page'
+  get 'structure/main_page'
+
+  root 'users#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
