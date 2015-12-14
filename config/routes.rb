951243b1505a2f0e6192941ecf696c48b1d7f913
Jorge <floresjmjr@gmail.com>
2015-12-14 18:09:52 -0500

    tweaked routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/show'
+  
 
   get 'structure/main_page'
 
+
   devise_for :users
+  resources :users, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
