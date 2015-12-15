71595dd307bf2460dd84557d2194a06b259bdbc0
Jorge <floresjmjr@gmail.com>
2015-12-14 20:17:51 -0500

    updated items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,39 @@
 class ItemsController < ApplicationController
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+
+    @item = Item.new(item_params)
+    @item.user = current_user
+
+    if @item.save
+      flash[:notice] = "Item was saved."
+      redirect_to items_show_path
+    else
+      flash [:error] = "There was an error saving the item to the list. Please try again."
+      render :new
+    end
+
+  end
+
+  def destroy
+
+    @item = Item.find(params[:id])
+
+    if @item.destroy
+      flash[:notice] = "\"#{@item}\" was deleted successuflly."
+      redirect_to items_show_path
+    else
+      flash[:error] = "There was an error deleting that item."
+      redirect_to items_show_path
+    end
+
+  end
 end
