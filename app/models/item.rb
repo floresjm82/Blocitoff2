f4cc11a0a27ca9ad6a656f275dcf8b8f8573884a
Jorge <floresjmjr@gmail.com>
2015-12-20 19:50:01 -0500

    updated Gem file and rspec command

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,11 @@
 class Item < ActiveRecord::Base
+  require 'action_view'
+  include ActionView::Helpers::DateHelper
+
   belongs_to :user
 
   validates_presence_of :name
+  validates :name, length: {maximum: 100}
 
 
 end
